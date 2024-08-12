79f40751bf94368c25dfd54659a46ba01d6fe11d
syahrul0505 <syahrulalif1234@gmail.com>
2024-08-12 21:53:23 +0700

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -57,7 +57,7 @@ const cloudPool = new Pool({
 
 setInterval(() => {
     RealtimeDashboardOrder()
-    syncAllOrdersAndRelatedTables()
+    // syncAllOrdersAndRelatedTables()
 }, 1000);
 
 const promisifiedLocalQuery = util.promisify(localPool.query).bind(localPool);
@@ -585,7 +585,7 @@ app.post('/sync-cloud-to-local', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
