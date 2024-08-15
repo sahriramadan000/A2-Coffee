fd950602b871ebfad87ed3b8854590b9ca4ad0a9
syahrul0505 <syahrulalif1234@gmail.com>
2024-08-15 10:38:56 +0700

    update

--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -194,14 +194,14 @@
             </li>
             @endcan
 
-            <li class="menu {{ request()->routeIs('sync.index') ? 'active' : '' }}">
+            {{-- <li class="menu {{ request()->routeIs('sync.index') ? 'active' : '' }}">
                 <a href="{{ route('sync.index') }}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-refresh-cw"><polyline points="23 4 23 10 17 10"></polyline><polyline points="1 20 1 14 7 14"></polyline><path d="M3.51 9a9 9 0 0 1 14.85-3.36L23 10M1 14l4.64 4.36A9 9 0 0 0 20.49 15"></path></svg>
                         <span>Sync</span>
                     </div>
                 </a>
-            </li>
+            </li> --}}
 
             {{-- <li class="menu">
                 <a href="#menuLevel1" data-bs-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
